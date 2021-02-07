05019d9b557b6364529672cb385f1b7ba5455291
r3pt1s <54471820+xXMcpeGamerXx03@users.noreply.github.com>
2021-02-07 11:54:06 +0100

    Add files via upload

---
+++ src/CUCM/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CUCM;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable()
+    {
+        $this->saveResource("config.yml");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCmd(PlayerCommandPreprocessEvent $event) {
+        $cfg = new Config($this->getDataFolder() . "config.yml", 2);
+        $player = $event->getPlayer();
+        $msg = explode(" ", $event->getMessage());
+        if ($event->getMessage()[0] == '/') {
+            $command = str_replace("/", "", $msg[0]);
+            if (Server::getInstance()->getCommandMap()->getCommand($command) == null) {
+                $event->setCancelled();
+                $player->sendMessage($cfg->get("Message"));
+            }
+        } else if ($event->getMessage()[0] == '.' && $event->getMessage()[1] == '/') {
+            $command = str_replace([".", "/"], ["", ""], $msg[0]);
+            if (Server::getInstance()->getCommandMap()->getCommand($command) == null) {
+                $event->setCancelled();
+                $player->sendMessage($cfg->get("Message"));
+            }
+        } else {
+            return;
+        }
+    }
+}
